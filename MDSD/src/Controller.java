bbdd5959b74ec7070ba80f31ee8090856f5bb81d
bscong <benoit.zhong@gmail.com>
2017-11-05 17:43:44 +0100

    Convert to Maven project, implemented Simbad

---
+++ MDSD/src/Controller.java
@@ -0,0 +1,19 @@
+import java.util.Set;
+
+import project.AbstractRobot;
+import project.SimulatorController;
+import simbad.sim.EnvironmentDescription;
+
+public class Controller extends SimulatorController {
+
+	public Controller(Set<? extends AbstractRobot> robots, EnvironmentDescription e) {
+		super(robots, e);
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ MDSD/src/Robot.java
@@ -0,0 +1,9 @@
+import project.AbstractRobot;
+import project.Point;
+
+public class Robot extends AbstractRobot {
+	public Robot(Point position, String name) {
+        super(position, name);
+	}
+
+}
