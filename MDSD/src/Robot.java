c02f5ddfcbfe4bbb5ee3b572d9f728d9e927777a
Pierre-Louis Janand <pljanand@gmail.com>
2017-11-21 14:37:54 +0100

    Update project

--- MDSD/src/Robot.java
+++
@@ -1,9 +0,0 @@
-import project.AbstractRobot;
-import project.Point;
-
-public class Robot extends AbstractRobot {
-	public Robot(Point position, String name) {
-        super(position, name);
-	}
-
-}
