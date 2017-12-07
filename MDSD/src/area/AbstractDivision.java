079d9b866e4d8375b700812dae6486c08f2cb0d5
Pierre-Louis Janand <pljanand@gmail.com>
2017-12-07 14:04:09 +0100

    Adding the hospital environment.

---
+++ MDSD/src/area/AbstractDivision.java
@@ -0,0 +1,13 @@
+package area;
+
+import java.util.Set;
+
+import project.Boundary;
+
+public abstract class AbstractDivision extends PhysicalArea {
+
+	public AbstractDivision(Set<Boundary> boundaries) {
+		super(boundaries);
+	}
+
+}
---
+++ MDSD/src/area/ConsultingRoom.java
@@ -0,0 +1,13 @@
+package area;
+
+import java.util.Set;
+
+import project.Boundary;
+
+public class ConsultingRoom extends AbstractRoom {
+	
+	public ConsultingRoom(Set<Boundary> boundaries, AbstractDivision division) {
+		super(boundaries, division);
+	}
+	
+}
